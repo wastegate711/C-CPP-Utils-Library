dd6a4ecaae370465bdee892523a525100f628943
wastegate711 <evgeni.shvec@gmail.com>
2022-09-19 14:59:04 +0300

    fgh
    dfgd

---
+++ TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/DependInfo.cmake
@@ -0,0 +1,86 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Core/Startup/startup_stm32f407vgtx.s" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Core/Startup/startup_stm32f407vgtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Core/Src/I2C_Lcd.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Core/Src/I2C_Lcd.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Core/Src/gpio.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Core/Src/gpio.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Core/Src/i2c.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Core/Src/i2c.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Core/Src/main.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Core/Src/main.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Core/Src/stm32f4xx_hal_msp.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Core/Src/stm32f4xx_it.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Core/Src/stm32f4xx_it.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Core/Src/syscalls.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Core/Src/syscalls.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Core/Src/sysmem.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Core/Src/sysmem.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Core/Src/system_stm32f4xx.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Core/Src/system_stm32f4xx.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "D:/Project_CLion/C_CPP_Utils_Library/TestProject/TestProject/cmake-build-debug-mingw/CMakeFiles/TestProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DEBUG"
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
